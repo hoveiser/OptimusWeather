1ef0d84f0431b9217f5d29ff4ddfe9937bd48aa4
oveis beheshti <Oveis.Beheshti.av@ia-grp.com>
2023-06-03 11:13:30 +0330

    first commit

---
+++ src/main/java/com/inpress/optimusweather/OptimusWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.inpress.optimusweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimusWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimusWeatherApplication.class, args);
+    }
+
+}
