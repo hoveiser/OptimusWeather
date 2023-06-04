dc481382e075c7d8f0bd8f8c168b0344a2a10dc2
oveis beheshti <Oveis.Beheshti.av@ia-grp.com>
2023-06-05 02:49:04 +0330

    add some comment

--- src/test/java/com/inpress/optimusweather/service/dto/WeatherServiceTest.java
+++ src/test/java/com/inpress/optimusweather/service/dto/WeatherServiceTest.java
@@ -10,14 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 //@Testcontainers
 class WeatherServiceTest {
 
+    /**
+     * need test for api using mockMvc to handle data is the same style as before
+     */
     @Test
     void getCurrentWeather() {
     }
 
+    /**
+     * need test for api using mockMvc to handle data is the same style as before
+     */
     @Test
     void getForecastWeather() {
     }
 
+    /**
+     * need test for api using mockMvc to handle data is the same style as before and also need to check unique constraint by test container
+     * or in memory db and also check some functionality for get part of date from db and get others from api
+     */
     @Test
     void getHistoricalWeather() {
     }
